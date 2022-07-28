690a9c3956e9536390f77d7d3c237f06a4e43eb8
JamesB <James.buchmann@gmail.com>
2022-07-28 07:37:22 -0500

    add function that reads and receives

--- routes/api_routes.js
+++ routes/api_routes.js
@@ -1,10 +1,26 @@
 
-
 const notes_router = require('express').Router();
 
-notes_router.get('/notes', (request, response) => {
 
+// CRUD
+// write the function that reads the notes from the DB
+
+function getNoteDB(){
+    return fs.promises.readFile(db_path, 'utf8')
+        .then(data => JSON.parse(data))
+       
+}
+// READ
+notes_router.get('/notes', (request, response) => {
+    getNoteDB()
+        .then(notes => response.json(notes))
+        .catch(err => console.log(err));
+
+        console.log('recieved the notes mate')
 })
+
+// UPDATE
+
 
 
 
