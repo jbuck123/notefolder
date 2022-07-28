0c6aed0f076ef65f17c02bcf36a7006c4e341f76
JamesB <James.buchmann@gmail.com>
2022-07-28 08:10:21 -0500

    finishing the server.js file

--- server.js
+++ server.js
@@ -3,10 +3,15 @@ const express = require('express');
 const PORT = process.env.PORT || 4321;
 const app = express();
 const path = require('path')
-
+// includes the api route in the server.
 const api_routes = require('./routes/api_routes')
 
+// joining the server.js with the public file 
 app.use(express.static(path.join(__dirname, 'public')));
+// attach form data to request.body object 
+app.use(express.urlencoded({extended: true}));
+//all express to parse JSON data 
+app.use(express.json())
 
 // display the homepage
 app.get('/', (request, response) => {
@@ -17,7 +22,7 @@ app.get('/notes', (request, response) =>{
     response.sendFile(__dirname + '/Develop/public/notes.html');
 })
 
-
+//localhost:4321/api
 app.use('/api', api_routes)
 
 app.listen(PORT, () => {
