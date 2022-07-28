7b13143de51d772ba5903eec5685f3e12c972878
JamesB <James.buchmann@gmail.com>
2022-07-28 07:56:00 -0500

    post method
    added all the requires to the top of the page

--- routes/api_routes.js
+++ routes/api_routes.js
@@ -1,5 +1,9 @@
-
 const notes_router = require('express').Router();
+const fs = require("fs");
+const path = require('path');
+const uuid = require('uuid').v4;
+// const mysql = require('mysql2');
+const db_path = path.join(__dirname, "../db/db.json")
 
 
 // CRUD
@@ -13,13 +17,33 @@ function getNoteDB(){
 // READ
 notes_router.get('/notes', (request, response) => {
     getNoteDB()
-        .then(notes => response.json(notes))
+        .then(notes_data => response.json(notes_data))
         .catch(err => console.log(err));
 
         console.log('recieved the notes mate')
 })
 
-// UPDATE
+// UPDATE 
+
+notes_router.post('/notes', (request, response) => {
+    getNoteDB()
+        .then((notes_data) => {
+            const new_note = request.body;
+            // using uuid to create unique ids for each note
+            new_note.id = uuid().slice(0,4);
+
+
+            notes_data.push(new_note);
+
+            // write the note to the DB
+
+            fs.promises.writeFile(db_path)
+
+            console.log(notes_data)
+
+        })
+})
+
 
 
 
