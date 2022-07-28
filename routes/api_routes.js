c7da6aa852718c4c3809528aa23d5fbf223b4da5
JamesB <James.buchmann@gmail.com>
2022-07-28 07:59:05 -0500

    writing the file

--- routes/api_routes.js
+++ routes/api_routes.js
@@ -37,9 +37,12 @@ notes_router.post('/notes', (request, response) => {
 
             // write the note to the DB
 
-            fs.promises.writeFile(db_path)
-
-            console.log(notes_data)
+            fs.promises.writeFile(db_path, JSON.stringify(notes_data, null, 2))
+                .then(() => {
+                    console.log('added successfully')
+                    response.json(notes_data)
+                })
+                .catch(err => console.log(err))
 
         })
 })
